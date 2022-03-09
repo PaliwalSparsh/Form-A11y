082f4061dd9334b882cbd95034fe450936ca27fe
Sparsh Paliwal <sparshpaliwal123@gmail.com>
2022-03-09 12:09:39 -0500

    feature: Restrict creation of annotation with small bounds

--- ui/src/context/AnnotationStore.ts
+++ ui/src/context/AnnotationStore.ts
@@ -99,7 +99,7 @@ export class PdfAnnotations {
     }
 
     withNewAnnotation(a: Annotation): PdfAnnotations {
-        console.log('Logging newly created annotation:', this.annotations);
+        console.log('Logging newly created annotation:', a);
         return new PdfAnnotations(this.annotations.concat([a]), this.relations, true);
     }
 
